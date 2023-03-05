d220ecf7fd0c56a42e0b03f66096ddbf196f4663
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-05 14:39:17 +0100

    Update pylaunch.py

--- pylaunch.py
+++ pylaunch.py
@@ -3,6 +3,8 @@ import tkinter as tk
 # create a Tkinter window
 window = tk.Tk()
 
+
+
 # define a function to be called when the button is clicked
 def launch_command():
     command = text_input.get()
@@ -12,17 +14,32 @@ def launch_command():
 # create a label and setup arguments for the text input
 window.geometry("300x500")
 window.configure(background="black")
-label = tk.Label(window, text="Command:", bg="black", fg="white")
+labelmain = tk.Label(window, text="Command:", bg="black", fg="white")
 
-text_input = tk.Entry(window, width=22)
+file_input = tk.Entry(window, width=22)
 
 # create a button widget
-button = tk.Button(window, text="Add file", width=8, height=1, bg="black", border=0)
+buttonmain = tk.Button(window, text="Add file", width=8, height=1, bg="white", border=0)
 
 # use the grid() method to position the widgets in a grid
-label.grid(row=0, column=0, padx=5, pady=5)
-text_input.grid(row=0, column=1, padx=5, pady=5)
-button.grid(row=0, column=2, padx=5, pady=5)
+labelmain.grid(row=0, column=0, padx=5, pady=5)
+file_input.grid(row=0, column=1, padx=5, pady=5)
+buttonmain.grid(row=0, column=2, padx=5, pady=5)
+
+
+i = 1
+testlist = ["apple", "banana", "cherry"]
+
+for item in testlist:
+    labellist = tk.Label(window, text=item, bg="black", fg="white")
+    buttonlist = tk.Button(window, text="Add file", width=8, height=1, bg="white", border=0)
+    labellist.grid(row=i, column=0, padx=5, pady=5)
+    buttonlist.grid(row=i, column=1, padx=5, pady=5)
+    i = i + 1
+
+labellist.grid(row=1, column=0, padx=5, pady=5)
+buttonlist.grid(row=1, column=1, padx=5, pady=5)
+
 
 # start the Tkinter event loop
 window.mainloop()
