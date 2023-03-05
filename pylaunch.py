0c88634a72f57e6fe6bf777425b0e425aacc7f02
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-05 14:01:13 +0100

    Update pylaunch.py

--- pylaunch.py
+++ pylaunch.py
@@ -5,25 +5,24 @@ window = tk.Tk()
 
 # define a function to be called when the button is clicked
 def launch_command():
-    command = entry.get()
+    command = text_input.get()
     print("Launching command:", command)
 
 
 # create a label and setup arguments for the text input
 window.geometry("300x500")
 window.configure(background="black")
-label = tk.Label(window, text="Command:", bg="black", fg="white", padx=20, pady=10)
+label = tk.Label(window, text="Command:", bg="black", fg="white")
 
-# create a text input widget
-entry = tk.Entry(window)
+text_input = tk.Entry(window)
 
-# create a button to launch the command
-button = tk.Button(window, text="Launch Command", command=launch_command)
+# create a button widget
+button = tk.Button(window, text="Submit")
 
-# add the widgets to the window
-label.pack()
-entry.pack()
-button.pack()
+# use the grid() method to position the widgets in a grid
+label.grid(row=0, column=0, padx=5, pady=5)
+text_input.grid(row=0, column=1, padx=5, pady=5)
+button.grid(row=0, column=2, padx=5, pady=5)
 
 # start the Tkinter event loop
 window.mainloop()
