c7beb545978a74680c3d42de4036209c3949b1d7
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-07 20:59:32 +0100

    prompt testing

--- pylaunch.py
+++ pylaunch.py
@@ -1,15 +1,50 @@
 import tkinter as tk
+import time
+import os
 
 # create a Tkinter window
 window = tk.Tk()
 
 
+filename = "config.txt"
+
+
+with open(filename, "r") as f:
+    lines = f.readlines()
+
+linecount = sum(1 for line in lines)
+print(linecount)
+
 
 # define a function to be called when the button is clicked
-def launch_command():
-    command = file_input.get()
-    labelitems = tk.Label(window, text="done" + command, bg="black", fg="white")
-    labelitems.grid(row=1, column=1, padx=5, pady=5)
+def getinput():
+    with open(filename, "r") as f:
+      lines = f.readlines()
+    
+
+    filepath = file_input.get()
+
+    for line in lines:
+        if line == "":
+            lines[line] = filepath + "\n"
+    
+
+
+    with open(filename, "w") as file:
+        file.writelines(lines)
+
+
+
+
+def firescript(argument):
+    print(argument)
+
+    if i <= len(lines):
+        line = lines[argument[0]].strip()
+        os.system("python3.10 " + line)
+    else:
+        print("No such line in config")
+
 
 
 # create a label and setup arguments for the text input
@@ -20,7 +55,7 @@ labelmain = tk.Label(window, text="Command:", bg="black", fg="white")
 file_input = tk.Entry(window, width=22)
 
 # create a button widget
-buttonmain = tk.Button(window, text="Add file", width=8, height=1, bg="white", border=0)
+buttonmain = tk.Button(window, text="Add file", width=8, height=1, bg="white", border=0, command=getinput)
 
 # use the grid() method to position the widgets in a grid
 labelmain.grid(row=0, column=0, padx=5, pady=5)
@@ -34,9 +69,9 @@ labelitems.grid(row=1, column=1, padx=5, pady=5)
 i = 2
 testlist = ["apple", "banana", "cherry"]
 #display items
-for item in testlist:
-    labellist = tk.Label(window, text=item, bg="black", fg="white")
-    buttonlist = tk.Button(window, text="Run File", width=8, height=1, bg="white", border=0)
+for argument in enumerate(testlist):
+    labellist = tk.Label(window, text=argument, bg="black", fg="white")
+    buttonlist = tk.Button(window, text="Run File", width=8, height=1, bg="white", border=0, command=lambda arg=argument: firescript(arg))
     labellist.grid(row=i, column=0, padx=0, pady=5)
     buttonlist.grid(row=i, column=2, padx=0, pady=5)
     i = i + 1
