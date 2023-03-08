4d42a837556ab3ec9896ead574722f862e53b422
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-08 21:31:00 +0100

    Update pylaunch.py

--- pylaunch.py
+++ pylaunch.py
@@ -1,49 +1,43 @@
 import tkinter as tk
-import time
 import os
 
 # create a Tkinter window
 window = tk.Tk()
-
-
 filename = "config.txt"
+
+
 
 
 with open(filename, "r") as f:
     lines = f.readlines()
 
 linecount = sum(1 for line in lines)
-print(linecount)
+
 
 
 # define a function to be called when the button is clicked
 def getinput():
     with open(filename, "r") as f:
       lines = f.readlines()
-    
 
     filepath = file_input.get()
 
     for line in lines:
         if line == "":
             lines[line] = filepath + "\n"
-    
-
 
     with open(filename, "w") as file:
         file.writelines(lines)
 
 
-
-
+# define a function to be called when the run button is clicked
 def firescript(argument):
-    print(argument)
+    line = lines[argument].strip()
 
-    if i <= len(lines):
-        line = lines[argument[0]].strip()
+    if line == "":
+        print("no such file")
+    else:       
         os.system("python3.10 " + line)
-    else:
-        print("No such line in config")
 
 
 
@@ -69,12 +63,14 @@ labelitems.grid(row=1, column=1, padx=5, pady=5)
 i = 2
 testlist = ["apple", "banana", "cherry"]
 #display items
-for argument in enumerate(testlist):
-    labellist = tk.Label(window, text=argument, bg="black", fg="white")
-    buttonlist = tk.Button(window, text="Run File", width=8, height=1, bg="white", border=0, command=lambda arg=argument: firescript(arg))
+for argument in enumerate(lines):
+    name = argument[1]
+    labellist = tk.Label(window, text=name, bg="black", fg="white")
+    buttonlist = tk.Button(window, text="Run File", width=8, height=1, bg="white", border=0, command=lambda arg=argument[0]: firescript(arg))
     labellist.grid(row=i, column=0, padx=0, pady=5)
     buttonlist.grid(row=i, column=2, padx=0, pady=5)
     i = i + 1
+
 
 
 
